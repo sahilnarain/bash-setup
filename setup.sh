04096782eae22c6f4d2b41bbef720e008922be3a
Sahil Narain <sahil.narain@gmail.com>
2016-06-23 13:38:11 +0000

    Detect OS

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+#Detect OS
+case "$OSTYPE" in
+   solaris*) OS="SOLARIS" ;;
+   darwin*)  OS="MAC" ;; 
+   linux*)   OS="LINUX" ;;
+   bsd*)     OS="BSD" ;;
+   *)        OS="Unknown" ;;
+esac
+
 
